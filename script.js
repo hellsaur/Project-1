96339b9b9d93d28deb0f1286dc9c8d76e81ea0ac
marco acosta <marcucho14@hotmail.com>
2017-10-08 20:45:14 -0400

    added files and changed css and html

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("main js is connected");
+
+let car1 = document.getElementById('car')
+let car2 = document.getElementById('car2')
+let button = document.getElementById("theButton");
+
+let c1 =0;
+let c2 =0;
+
+function go(race) {
+  let key = race.keyCode;
+    //car 1
+    if(key == 88) {
+        car1.style.transform = `translateX(${c1}px)`;
+      c1+= 10;
+    }
+    //car 2
+    if(key == 90) {
+     car2.style.transform = `translateX(${c2}px)`;
+      c2+= 10;
+    }
+   winner();
+}
+
+window.addEventListener('keyup', go);
+
+function winner(){
+  if(c1 + 450 === 1200){
+    alert("car1is the Winner!!!");
+    window.removeEventListener("keyup", go);
+  }
+  else if(c2 +450 === 1200){
+    alert("car2 is the Winner!!!");
+    window.removeEventListener("keyup", go);
+  }
+}
+
