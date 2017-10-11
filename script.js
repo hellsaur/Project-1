07e257d4154baf6ea84754c2450aae5f751bae80
marco acosta <marcucho14@hotmail.com>
2017-10-11 19:19:09 -0400

    created a new file plus collision

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("main js is connected");
 
-let car1 = document.getElementById('car')
-let car2 = document.getElementById('car2')
+let car1 = document.getElementById('car');
+let car2 = document.getElementById('car2');
 let bg = document.querySelector(".container");
 let finish = document.querySelector(".finishLine");
 let winr = document.querySelector(".player1");
@@ -11,7 +11,20 @@ let wheel2 = document.querySelector("#wheel2");
 let wheel3 = document.querySelector("#wheel3");
 let starB = document.querySelector(".starB");
 let replayB = document.querySelector(".replayB");
-
+let landingB = document.querySelector('.landingB');
+let object = document.querySelector('#object');
+objectX = object.offsetLeft;
+objectY = object.offsetTop;
+objectH = object.offsetHeight ;
+objectW = object.offsetWidth ;
+car1X = car1.offsetLeft ;
+car1Y = car1.offsetTop;
+car1H = car1.offsetHeight;
+car1W = car1.offsetWidth ;
+car2Y = car2.offsetTop;
+car2H = car2.offsetHeight;
+car2W = car2.offsetWidth;
+car2X = car2.offsetLeft ;
 let c1 =0;
 let c2 =0;
 
@@ -21,40 +34,79 @@ function go(race) {
   let key = race.keyCode;
     //car 1
     if(key == 83) {
-        car1.style.transform = `translateX(${c1}px)`;
-      c1+= 10;
+     car1.style.left = (car1X+=10) + "px";
     }
     //car 2
     if(key == 74) {
-     car2.style.transform = `translateX(${c2}px)`;
-      c2+= 10;
+    car2.style.left = (car2X+=10) + "px";
     }
     if(key == 87) {
-     car1.style.transform = `translateY(${c1}px)`;
-      c1+= 10;
+     car1.style.top = (car1X+=10) + "px";
     }
        if(key == 88) {
-     car1.style.transform = `translateY(${c1}px)`;
-      c1-= 10;
+     car1.style.top = (car1X-=10) + "px";
     }
      if(key == 85) {
-     car2.style.transform = `translateY(${c2}px)`;
-      c2+= 10;
+     car2.style.top= (car1X+=10) + "px";
     }
        if(key == 77) {
-     car2.style.transform = `translateY(${c2}px)`;
-      c2-=10;
+     car2.style.top = (car1X-=10) + "px";
     }
+  collision();
    winner();
 }
 
-window.addEventListener('keyup', go);
+
+
 // this specifies when the winner reaches a certain amonut of pixels it will display the image.
+let deleted = false;
+function collision (){
+if(deleted == false){
+
+  if ((car1X + car1W) > objectX && car1X< (objectX+objectW)){
+     car1.style.left = (car1X=0) + "px";
+     clearInterval(object);
+    deleted = true;
+
+    alert("player 1 crashed")
+  }
+
+  if((car1Y + car1H) > objectY && car1Y < (objectY+objectH)){
+ car1.style.left = (car1X=0) + "px";
+     clearInterval(object);
+    deleted = true;
+
+    alert("player 1 crashed")
+
+  }
+  if ((car2X + car2W) > objectX && car2X< (objectX+objectW) && (car2Y + car2H) > objectY && car2Y < (objectY+objectH)){
+    car2.style.left = (car2X=0) + "px";
+  alert("player 2 crashed")
+   deleted = true;
+  }
+
+}
+
+ window.requestAnimationFrame(collision);
+
+}
+
+ function car3(){
+
+    if(objectX ===  -500){
+      clearInterval();
+
+    } else {
+      object.style.left = (objectX  -= 1) + "px";
+
+    }
+
+ }
 
 
 function winner(){
 
-  if(c1 + 500 === 1400){
+  if(car1X + 500 === 1320){
     bg.style.animationPlayState = "paused";
    wheel1.style.animationPlayState = "paused";
    wheel2.style.animationPlayState = "paused";
@@ -66,7 +118,7 @@ function winner(){
 
   }
 
-  else if(c2 +500 === 1400){
+  else if(car2X +500 === 1320){
    bg.style.animationPlayState = "paused";
    wheel1.style.animationPlayState = "paused";
    wheel2.style.animationPlayState = "paused";
@@ -75,8 +127,8 @@ function winner(){
    winr2.style.display = "block";
   playAudio()
   window.removeEventListener("keyup", go);
-
-  }
+  console.log("car2 end")
+}
 
 }
 
@@ -93,11 +145,18 @@ function startGame(){
 location.reload()
 
 }
+// glitches when i use event listener on this button.
+// replayB.addEventListener('click', startGame());
+
 function start(){
 bg.style.animationPlayState = "running";
 wheel1.style.animationPlayState = "running";
 wheel2.style.animationPlayState = "running";
 wheel3.style.animationPlayState = "running";
 wheel4.style.animationPlayState = "running";
+window.addEventListener('keyup', go);
+
+let ob = setInterval(car3,3)
 
 }
+
